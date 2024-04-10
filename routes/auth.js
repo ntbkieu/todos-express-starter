59a5111c33b131aa5aa1a316b67804f0c695562d
ntbkieu <159548547+ntbkieu@users.noreply.github.com>
2024-04-10 09:11:11 +0700

    verify password

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,41 @@
 var express = require('express');
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var crypto = require('crypto');
+var db = require('../db');
 var router = express.Router();
 
-router.get('/login', function(req, res, next) {
-  res.render('login');
+/** Verify Password (2)
+ * Configures the LocalStrategy
+ * -  to fetch the user record from the app's database
+ * -  verify the hashed password against the password submitted by the use
+ * 
+ * true: the password is valid and the user is authenticated.
+ */
+passport.use(new LocalStrategy(function verify(username, password, cb) {
+    db.get('SELECT * FROM users WHERE username = ?', [username], function (err, row) {
+        if (err) { return cb(err); }
+        if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+
+        crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function (err, hashedPassword) {
+            if (err) { return cb(err); }
+            if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+                return cb(null, false, { message: 'Incorrect username or password.' });
+            }
+            return cb(null, row);
+        });
+    });
+}));
+
+router.get('/login', function (req, res, next) {
+    res.render('login');
 });
+
+// route that will authenticate the user
+// when submit the form by clicking "Sign in."
+router.post('/login/password', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
 
 module.exports = router;
